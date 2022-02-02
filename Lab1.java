a9a4f3615ee859d8048ff63b39370e7e6a701110
misskushkush <88245223+misskushkush@users.noreply.github.com>
2022-02-02 23:51:19 +0200

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,23 @@
+package javadevelop.oop;
+
+public class Lab1 {
+    public static void main(String[] args) {
+       final long a = 1;
+       final long b = 0;
+       final long m = 2;
+       final long n = 3;
+       final  long C = 2;
+       double S = 0;
+
+        for (long i = a; i <= n; i++) {
+            if (i+C==0) {
+                System.out.println("Error! Division by zero!");
+                break;
+            }
+            for (long j = b; j <= m; j++) {
+                S += (double)(i+j)/(i+C);
+            }
+        }
+        System.out.println("S = "+ S);
+    }
+}
