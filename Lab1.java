ff2bcd2fc91c30418670d76c7cbfa35ea8fe9568
misskushkush <88245223+misskushkush@users.noreply.github.com>
2022-02-06 20:09:57 +0200

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -6,9 +6,9 @@ public class Lab1 {
        final long b = 0;
        final long m = 2;
        final long n = 3;
-       final  long C = 2;
+       final long C = 2;
        double S = 0;
-
+       if ((a<=n)&&(b<=m)) {
         for (long i = a; i <= n; i++) {
             if (i+C==0) {
                 System.out.println("Error! Division by zero!");
@@ -18,6 +18,8 @@ public class Lab1 {
                 S += (double)(i+j)/(i+C);
             }
         }
-        System.out.println("S = "+ S);
+           System.out.println("S = "+ S);
+       }
+       else System.out.print("Error! The limits are smaller than the begging numbers!");
     }
 }
